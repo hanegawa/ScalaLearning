8486bfa905126470bd5fb0605ae9715aa414cd64
hanegawa <936924308@qq.com>
2020-05-23 20:06:50 +0800

    GitForScala测试2

--- src/List_map.scala
+++ src/List_map.scala
@@ -3,5 +3,7 @@ object List_map{
     val list = List(1, 2, 3)
     val f = (i:Int)=>println(i)
     list.foreach(f)
+    val univresity = Map("XMU"->"Xiamen University", "THU"->"Tsinghua University")
+    univresity foreach{kv => println(kv._1 + ":" + kv._2)}
   }
 }
