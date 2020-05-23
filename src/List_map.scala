a5dc55cfdeed311920de9277a39e063d03ba4b13
hanegawa <936924308@qq.com>
2020-05-23 20:25:56 +0800

    GitForScala测试2

--- src/List_map.scala
+++ src/List_map.scala
@@ -3,7 +3,18 @@ object List_map{
     val list = List(1, 2, 3)
     val f = (i:Int)=>println(i)
     list.foreach(f)
-    val univresity = Map("XMU"->"Xiamen University", "THU"->"Tsinghua University")
-    univresity foreach{kv => println(kv._1 + ":" + kv._2)}
+    val university = Map("XMU"->"Xiamen University", "THU"->"Tsinghua University")
+    university foreach{kv => println(kv._1 + ":" + kv._2)}
+//    university foreach{x=>x match {case (k, v) => println(k + ":" + v)}}
+    university foreach{case (k, v) => println(k + ":" + v)}
+    val books = List("Hadoop", "Hive", "HDFS")
+    val length = books.map(s => s.length)
+    print(length)
+    books foreach{item => println(item)}
+
+    //flatmap:一对多映射
+    val newList = books flatMap(s => s.toList)
+    print(newList)
+    newList foreach(item => println(item))
   }
 }
